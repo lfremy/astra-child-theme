b98e57b80555ad45e2a458b9bc0957a59c8cac3f
lfremy <75318984+lfremy@users.noreply.github.com>
2021-03-18 16:16:12 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# astra-child-theme
+# Astra Child for WordPress Workshop (Blogtrotter)
+
+Fork & clone this repo to your wp-content/themes
---
+++ functions.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Theme functions and definitions
+ */
+
+// load stylesheets in proper order
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function my_theme_enqueue_styles() {
+	wp_enqueue_style( 'astra-theme-css', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style(
+		'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'astra-theme-css' ),
+		wp_get_theme()->get( 'Version' )
+	);
+}
+
+// load theme text domain
+add_action( 'after_setup_theme', 'astra_child_simplon_theme_setup' );
+
+function astra_child_simplon_theme_setup() {
+	load_child_theme_textdomain( 'astra-child-simplon' );
+}
+
+// add custom section to customizer
+
+add_action(
+	'customize_register',
+	'astra_child_simplon_add_stuff_to_customizer'
+);
+
+function astra_child_simplon_add_stuff_to_customizer( $wp_customize ) {
+	$wp_customize->add_section(
+		'astra-child-section',
+		array(
+			'title' => __( 'Astra Child Theme Settings', 'astra-child-simplon' ),
+		)
+	);
+	$wp_customize->add_setting(
+		'astra_child_fixed_navbar',
+		array(
+			'default' => true,
+			function( $checked ) {
+					return ( ( isset( $checked ) && true == $checked ) ? true : false );
+			},
+		)
+	);
+	$wp_customize->add_control(
+		'astra_child_fixed_navbar',
+		array(
+			'type'        => 'checkbox',
+			'section'     => 'astra-child-section', // Required, core or custom.
+			'label'       => __( 'Fixed navigation bar', 'astra-child-simplon' ),
+			'description' => __( 'Check here if you want your navigation bar fixed to the top of the screen.', 'astra-child-simplon' ),
+		)
+	);
+}
+
+// add custom class to body
+
+add_filter( 'body_class', 'astra_child_simplon_add_body_class' );
+
+function astra_child_simplon_add_body_class( $classes ) {
+	if ( get_theme_mod( 'astra_child_fixed_navbar' ) ) {
+		 $classes[] = 'sticky-navbar';
+	}
+
+	return $classes;
+}
+
+/* your code starts here 👇 */
+
+// add custom color palette to the block editor
+
+// Editor Color Palette
+add_theme_support( 'editor-color-palette', array(
+	array(
+		'name'  => __( 'Carbon', 'astra-child-simplon' ),
+		'slug'  => '.has-carbon-color,',
+		'color'	=> '#4A4A4A',
+	),
+  array(
+		'name'	=> __( 'Caramel', 'astra-child-simplon' ),
+		'slug'	=> '.has-caramel-color,',
+		'color'	=> '##8B572A',
+	),
+	array(
+		'name'  => __( 'Vanilla', 'astra-child-simplon' ),
+		'slug'  => '.has-vanilla-color',
+		'color'	=> '#EBE5D9',
+	),
+	array(
+		'name'  => __( 'Cappucino', 'astra-child-simplon' ),
+		'slug'  => '.has-cappucino-color',
+		'color' => '#B09F82',
+	),
+	array(
+		'name'  => __( 'white', 'astra-child-simplon' ),
+		'slug'  => '.has-white-color',
+		'color' => '#FFFFFF',
+	),
+	
+) );
+
